df57cce25a1d7578b3fc2867a2b94176bafbe3d0
Rafa Tafol <rafatafol26@gmail.com>
2025-12-19 15:48:57 +0100

    Primera subida el fichero Calculadora con help implementado

---
+++ Calculadora.java
@@ -0,0 +1,87 @@
+public class Calculadora {
+
+	public static void main(String[] args){
+
+		if (args.length == 1 && args[0].equals("help")) {
+
+			help();
+			System.exit(0);
+
+		}
+
+
+		if (args.length != 3){
+			System.out.println("Numero de parametros incorrectos");
+			System.out.println("USO: Java Calculadora <operacion> num1 num2");
+			//Esto es para salir fuera cuando salte eso.
+			System.exit(-1);
+		}
+
+		String operacion = args[0];
+		double num1 = Double.parseDouble(args[1]);
+		double num2 = Double.parseDouble(args[2]);
+		double resultado;
+
+	
+
+		switch (operacion) {
+			case "sumar" -> resultado = sumar(num1,num2);
+			case "restar" -> resultado = restar(num1,num2);
+			case "mult" -> resultado = mult(num1,num2);
+			case "div" -> resultado = div(num1,num2);
+			default -> throw new RuntimeException("Operacion elegida no soportada");
+			//Se cierra con ";" porque es como una expresion, no es el case normal.
+		};
+		
+
+	//Esto es para imprimir con formato, float y double con printf es lo mismo, estamos diciendole, que tendra 2 decimales, % sustituye, y la "s" es un string
+	//Lo que va entre comillas es una "expresion para sustituir) y luego lo de despues es en orden ir diciendo a que va a sustituir.
+	System.out.printf("%.2f %s %.2f = %.2f\n", num1, operacion, num2, resultado);
+	
+	}
+
+
+
+
+	public static void help(){
+
+		System.out.println("USO: Java calculadora <operacion> num1 num2");
+		System.out.println("Operaciones soportadas");
+		System.out.println("sumar");
+		System.out.println("restar");
+		System.out.println("mult");
+		System.out.println("div");
+
+	}
+
+
+	public static double sumar(double num1, double num2){
+
+		return num1 + num2;
+
+	}
+
+
+	public static double restar(double num1, double num2){
+
+		return num1 - num2;
+	
+	}
+
+
+	public static double mult(double num1, double num2){
+
+
+		return num1 * num2;
+
+	}
+
+	public static double div(double num1, double num2){
+
+
+		return num1 / num2;
+
+	}
+
+
+}
